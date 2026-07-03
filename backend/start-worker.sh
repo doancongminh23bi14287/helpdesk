9bcb7cd694597e0f027cf18f5987ab08851504b9
doancongminh23bi14287 <acm12112005n@gmail.com>
2026-07-03 16:27:04 +0700

    reduce celery worker memory usage with solo pool

--- backend/start-worker.sh
+++ backend/start-worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 echo "=== STARTING CELERY WORKER + BEAT ==="
-celery -A app.tasks.celery_app worker --beat --loglevel=info
+celery -A app.tasks.celery_app worker --beat --loglevel=info --concurrency=1 --pool=solo
